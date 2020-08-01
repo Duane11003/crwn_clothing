a410bd37fb9ac729664795cdf332bf8b0ab54a4a
Duane McFarlane <duane.mcfar@gmail.com>
2020-08-01 12:42:28 -0400

    added scss styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Homepage from './Homepage'
 
 function App() {
     return (
         <div>
-            <p>Hello World!!!</p>
+            <Homepage />
         </div>
     )
 }
